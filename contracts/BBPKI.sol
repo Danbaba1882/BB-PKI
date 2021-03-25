fe788067306ebb964798bb800dfc065ee01ce985
Usman Danbaba <usmandanbaba258@yahoo.com>
2021-03-25 15:51:00 +0100

    add initial commit

--- contracts/BBPKI.sol
+++ contracts/BBPKI.sol
@@ -1,21 +1,26 @@
-pragma solidity >=0.4.22 <0.9.0;
+
+pragma solidity ^0.8.3;
+
+import "unixtotime.sol";
 
 contract Bbpki {
 
 // state variables
 
 address owner;
-mapping (uint => certificateInfo) public certificates;
-mapping (uint => infoCA) public cAuthorities;
+mapping (uint => certificate) public certificates;
+mapping (uint => certificateAuthority) public cAuthorities;
 bytes32[] public caHashes;
 uint256 public noOfcertificates = 0;
 uint256 public noOfCA = 0;
-uint256 public serialNumber = 0;
 
 // constructor
-function Bbpki () {
-owner = msg.sender
+constructor () {
+owner = msg.sender;
 }
+
+ // enumerations (predetermined values)
+    enum certificateStatus {active, revoked, expired}
 
 // access restrictions
 modifier onlyOwner (){
@@ -23,110 +28,113 @@ require (owner == msg.sender);
     _;
 }
 
+
+
 // structures
 
     struct certificate {
-        uint serialNumber;
-        string subjectNamme;
-        string commonName;
+        uint256 serialNumber;
+        string subjectName;
         string organisation;
         string issuer;
         uint expiry;
-        uint noOfSignatures;
-        string status;
-        uint[] signatures;
+        uint256 noOfSignatures;
+        certificateStatus Status;
+        string[] signatures;
     }
 
     struct certificateAuthority {
     uint id;
     string nameCA;
-    bytes32 hash;
     }
 
-    // enumerations (predetermined values)
-    enum certificateStatus = {active, revoked, expired}
-     certificateStatus constant defaultStatus = certificateStatus.active;
+   
 
     // events
     // certificate authority registeration event
-    event caRegistered = (uint noOfCA, string memory _name );
+    event caRegistered (uint noOfCA, string _name );
 
     // certificate signing event
-    event certificateSigned(uint256 serialNumber,
-    string memory subjectName, 
-     string memory commonName,
-     string memory organisation,
-     string memory issuer,
+    event certificateSigned(
+    uint256 serialNumber,
+    string  subjectName, 
+     string  organisation,
+     string  issuer,
      uint256 expiry,
-     unit256 noOfSignatures,
-     string signatures[]
-     Status );
+     uint256 noOfSignatures,
+     certificateStatus Status,
+     string[] signatures
+      );
 
     // functions 
     function registerCA (string memory _name) public onlyOwner{
-        require ((bytes(_name).length > 0))
-        require(noOfCA < 10)
-        noOfRA++;
-        rAuthorities[noOfCA] = infoCA(noOfCA, _name);
-        emit caRegistered (noOfCA, _name)
+        require ((bytes(_name).length > 0));
+        require(noOfCA < 10);
+        noOfCA++;
+        cAuthorities[noOfCA] = certificateAuthority(noOfCA, _name);
+        emit caRegistered (noOfCA, _name);
     }
 
 
-    function signCertificate(string memory subjectName, string memory commonName, string memory organisation) public returns (
+    function signCertificate(string memory subjectName, string memory organisation) public returns (
      uint256 serialNumber,
-     string memory subjectName, 
-     string memory commonName,
-     string memory organisation,
+     string memory, 
+     string memory,
      string memory issuer,
      uint256 expiry,
-     unit256 noOfSignatures,
-     string signatures[]
-     string caReferences[];
-     certificateStatus defaultStatus; 
+     uint256 noOfSignatures,
+     certificateStatus,
+     string[] memory
+     
 
      ) {
+    serialNumber = block.timestamp/8;
+    noOfcertificates++;
 
     // generate a random number between 3 and 10 inclusive to determine the number of CA to sign the certificate automatically
-
-    uint256 caRequired = 
-    for (uint i = 0; i < caRequired.length; i++) 
+    uint caRequired = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % 9;
+    if (caRequired <3){
+        caRequired = caRequired + 3;
+        }
+        else {
+          caRequired = caRequired + 1;   
+        }
+    for (uint i = 1; i < caRequired; i++)
     {
-        signature = cAuthorities[i].hash; 
-        string memory signedBy = caAuthorities[i].hash  + "by" + caAuthorities[i].name 
-        signatures.push(signedBy);
-        caReferences.push(signature);
+    certificates[serialNumber].signatures.push(cAuthorities[i].nameCA);
     }
-    noOfcertificates++
+  
     // generate a random number between 1 and "caRequired" to select the issuer of the certificate
-    uint cIssuer = 
-    issuer = cAuthorities[cIssuer].name;
+    uint cIssuer = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % caRequired;
+    if (cIssuer < 1){
+        cIssuer = cIssuer + 1;
+        }
+    issuer = cAuthorities[cIssuer].nameCA;
     noOfSignatures = caRequired;
-    int256 serialNumber = 
+   expiry = block.timestamp + (365*24*60*60);
+ //(uint year,uint month,uint day) = BokkyPooBahsDateTimeLibrary.timestampToDate(a);
+    
+ certificateStatus Status = certificateStatus.active;
+certificates[serialNumber].serialNumber = serialNumber;
+certificates[serialNumber].subjectName = subjectName;
+certificates[serialNumber].organisation = organisation;
+certificates[serialNumber].issuer = issuer;
+certificates[serialNumber].expiry = expiry;
+certificates[serialNumber].noOfSignatures = noOfSignatures;
+certificates[serialNumber].Status = Status;
 
-
-    certificates[noOfcertificates] = certificateInfo(serialNumber, subjectName, commonName, organisation, issuer, expiry, noOfSignatures,signatures[], caReferences[], defaultStatus);
-
-     emit certificateSigned(serialNumber, subjectName, commonName, organisation, issuer, expiry, noOfSignatures,signatures[], caReferences[], defaultStatus );
-     return (serialNumber, subjectName, commonName, organisation, issuer, expiry, noOfSignatures,signatures[], caReferences[], defaultStatus );
+  //  certificates[noOfcertificates] = certificate(serialNumber, subjectName, organisation, issuer, expiry, noOfSignatures,defaultStatus,signatures);
+     emit certificateSigned(serialNumber, subjectName, organisation, issuer, expiry, noOfSignatures,Status,certificates[noOfcertificates].signatures);
+     return (serialNumber, subjectName, organisation, issuer, expiry, noOfSignatures,Status,certificates[noOfcertificates].signatures );
     }
 
-    function revokeCertificate (uint256 serialNumber) public onlyOwner returns (){
-        certificateStatus status = certificates[i].status;
-    }
-
-    function getCertificate(uint256 serialNumber) view public returns (address[] memory) { //Get all the info that has been added
-        return certAdd;
+    function revokeCertificate (uint256 serialNumber) public onlyOwner returns (string memory){
+     certificateStatus status = certificateStatus.revoked;
+     certificates[serialNumber].Status = status;
+    return "revoked";
     }
 
     function countCertificates() view public returns (uint){ //Count how many users create certificates
       return noOfcertificates;
-    }
-
-    function getCA (uint _id) public return (uint){
-        return 
-    }
-
-     function getRA (uint _id) public return (uint){
-        
     }
 }
