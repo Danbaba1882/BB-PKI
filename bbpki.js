5262b74e1aa8e2f6809dcbe69e2829d62c0435cf
Usman Danbaba <usmandanbaba258@yahoo.com>
2021-04-09 14:07:55 +0100

    add initial commit

--- bbpki.js
+++ bbpki.js
@@ -13,7 +13,6 @@ const server = express()
 const Web3 = require('web3')
 const { generateKeyPair } = require('crypto');
 const { GetAndVerify, GetProof, VerifyProof } = require('eth-proof')
-let getAndVerify = new GetAndVerify("https://mainnet.infura.io")
 var cas = JSON.parse(fs.readFileSync("cas.json"))
 var casArray = cas.cas;
 server.set('view engine', 'ejs');
@@ -29,59 +28,43 @@ const web3 = new Web3(RPC_URL)
 const abi = contract.abi;
 var pbkey;
 var prkey;
-const address = "0x9085EaFA70ae65e6292aeFd7a1BFF27717734Cc3"
-const BlockSSLcontract = new web3.eth.Contract(abi, address)
-//import {init, SecretKey, secretKeyToPublicKey, sign, verify} from "@chainsafe/bls";
+const contractAddress = "0x9085EaFA70ae65e6292aeFd7a1BFF27717734Cc3"
+const BlockSSLcontract = new web3.eth.Contract(abi, contractAddress)
 const bls = require('noble-bls12-381');
 const { version } = require('moment')
 
-// You can use Uint8Array, or hex string for readability
-const privateKey = '67d53f170b908cabb9eb326c3c337762d59289a8fec79f7bc9254b584b73265c';
+var certificate = {}
 
-
-/*var certificate = {
-  version: version,
-  SerialNumber: serialNumber,
-  SubjectName:  subjectName,
-  SubjectPublicKey: PublicKey,
-  Validity: pValidity,
-  issuer: issuer,
-  MultiSignatures: signatures2,
-  certificateSignature: aggSignature2,
-  sigVerify: isCorrect2,
-  certStatus: certStatus,
-  certBlockNUm: blockNumber
-} */
+// global variables
 var _serialNumber;
 var signatures2;
 var aggPubKey2;
 var aggSignature2;
 var isCorrect2;
-// sign certificate
-// bls multi-signature system, You can use Uint8Array, or hex string for readability
+// sign certificate function
+// utilizing the bls multi-signature system, You can use Uint8Array, or hex string for readability
 async function signCertificate(domain){
-
 const privateKey = '67d53f170b908cabb9eb326c3c337762d59289a8fec79f7bc9254b584b73265c';
 const domainName = domain;
+// domain conversion to hex string
 const domainHex = toHexString(domainName)
 const numCas = Math.random() * (11 - 3) + 3;
 const privateKeys = [];
- 
-    for (var i=0; i< numCas; i++){
+
+// getting and setting the private keys of all selected cas
+  for (var i=0; i< numCas; i++){
       const ca = casArray[i];
       const caPrivateKey = ca.privateKey;
       privateKeys.push(caPrivateKey);
     }
 const message = domainHex;
 
-
+// function that handles the bls signature process
 var as = (async () => {
   const publicKey = bls.getPublicKey(privateKey);
   const publicKeys = privateKeys.map(bls.getPublicKey);
-
   const signature = await bls.sign(message, privateKey);
   const isCorrect = await bls.verify(signature, message, publicKey);
-
 
   // Sign certifictae with private keys of CA's
   signatures2 = await Promise.all(privateKeys.map(p => bls.sign(message, p)));
@@ -95,98 +78,85 @@ var as = (async () => {
 
 
 return [signatures2, aggPubKey2, aggSignature2, isCorrect2];
-  // Sign 3 msgs with 3 keys
 })();
  
+// getting certificate parameters
 console.log(await as, signatures2)
-const currentts = Date.now();
-_serialNumber = currentts;
-const expiryts = parseInt(currentts) + (365*24*60*60);
-console.log(currentts,expiryts)
-const ex=  moment(expiryts).format('MM/DD/YYYY');
-console.log(ex)
-const iss  = parseInt(Math.random() * (numCas - 0) + 1);
-const issuer = casArray[iss].caName;
+const currentTime = Date.now();
+_serialNumber = currentTime;
+const expiryTime = parseInt(currentTime) + (365*24*60*60);
+const extString=  moment(expiryTime).format('MM/DD/YYYY');
+const issuer = parseInt(Math.random() * (numCas - 0) + 1);
+const certIssuer = casArray[issuer].caName;
 
-// parameters
+// setting certificate parameters
 const version = "X.509V3";
 const certStatus = true;
 const subjectname = "domainName";
-var multisigs = signatures2;
-const combsigs = aggSignature2;
-const expiry = expiryts;
-const issuerr = issuer;
+const multisigs = signatures2;
+const certSignature = aggSignature2;
+const expiry = expiryTime;
+const certIssuerr = certIssuer;
 const publicKey = aggPubKey2;
 
+certificate.version = version
+  certificate.SerialNumber = _serialNumber
+  certificate.SubjectName = subjectname
+  certificate.SubjectPublicKey = publicKey
+  certificate.Validity = expiry
+  certificate.certIssuer = certIssuer
+  certificate.MultiSignatures = signatures2
+  certificate.certificateSignature = aggSignature2
+  certificate.sigVerify = isCorrect2
+  certificate.certStatus = certStatus
+ // certificate.certBlockNUm = blockNumber
 
+  console.log(certificate);
 
-  const pvtkey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-  web3.eth.accounts.wallet.add("0x"+pvtkey);
-  const setCertificate = await BlockSSLcontract.methods.issueCertificate(version, currentts, subjectname, "JJJJ",expiry,issuerr,signatures2, combsigs, certStatus).send({
+//issuing certificate on the ethereum blockchain and transaction inclusion (for proof of existence and cert status)
+const pvtkey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+web3.eth.accounts.wallet.add(pvtkey);
+ const add =  web3.eth.accounts.wallet[0].address;
 
-    'from': '0xf0f6D0d5D540F5E639C594bBc6c6f57d903A02bC',
-    'gas':1000000,
-    value:0,
-  
-  
+const setCertificate = await BlockSSLcontract.methods.issueCertificate(version, currentTime, subjectname, "JJJJ",expiry,certIssuerr,multisigs, certSignature, certStatus).send({
+  'from': add,
+  'gas':1000000,
+  value:0
   }, function(error, data){
-    console.log(error);
+    if (error){
+      console.log(error);
+    }
     console.log(data)
   });
-  console.log('sssssssssssss', setCertificate)
+  console.log('certInfo', setCertificate)
 }
 
-
-
+// string to hex conversion function
 function toHexString(byteArray) {
   return Array.from(byteArray, function(byte) {
     return ('0' + (byte & 0xFF).toString(16)).slice(-2);
   }).join('')
 }
 
-// register CA
-async function registerCA(){
-  const testCA = "Digicert" // to be change on each function call up to the maximum numbr of CA required
-  const pvtkey = "fab65123befae2ad210633b072c7862bf7b68e0c65900xxxxxxxxxxxxxx" // wallet address private key
-  web3.eth.accounts.wallet.add("0x"+pvtkey);
-  const setCertificate = await BlockSSLcontract.methods.registerCA(_CAname).send({
-
-    'from': '0xf0f6D0d5D540F5E639C594bBc6c6f57d903A02bC',
-    'gas':1000000,
-      value:0,
-  
-  
-  }, function(err, data){
-    if (err){
-      res.send({errror: err})
-    }
-
-    else {
-      res.send({data: data})
-    }
-    
-  });
-  console.log('sssssssssssss', setCertificate)
-}
-
-
-
 // revoke certificate
 async function revokeCertificate(){
   const pvtkey = "fab65123befae2ad210633b072c7862bf7b68e0c659xxxxxxxxxxxxxxxxxxxx"
-  web3.eth.accounts.wallet.add("0x"+pvtkey);
-  const setCertificate = await BlockSSLcontract.methods.revokeCertificate(_serialNumber).send({
-
-    'from': '0xf0f6D0d5D540F5E639C594bBc6c6f57d903A02bC',
-    'gas':1000000,
-      value:0,
-  
-  
-  }, function(error, data){
-    console.log(error);
+var rawTransaction = {
+  'from': '0x9085EaFA70ae65e6292aeFd7a1BFF27717734Cc3',
+  'gas':1000000,
+  value:0
+  }
+  var signedtx = web3.eth.accounts.signTransaction(rawTransaction, pvtkey)
+  const revokeCert = await BlockSSLcontract.methods.revokeCertificate(_serialNumber).send(signedtx, function(error, data){
+    if (error){
+      console.log(error);
+    }
     console.log(data)
   });
-  console.log('sssssssssssss', setCertificate)
+  console.log('certStatus', revokeCert)
+  // setting certificate object
+
+  
 }
 
 //get certificate
@@ -195,9 +165,7 @@ async function  getCertificate() {
   console.log(certificate)
 }
 
-
-
-// client verify certificate
+// client verify certificate using the eth-proof
 async function clientVerifyCert() {
 const certificate = await BlockSSLcontract.methods.certificates(_serialNumber).call( async (err, result)=>{
 console.log(result)
@@ -206,10 +174,10 @@ const transaction = await web3.eth.getBlock(blockNumber);
 if (transaction) {
 const getnverify = new GetAndVerify(RPC_URL);
 const proof = getnverify.txAgainstBlockHash(txHash, trustedBlockHash);
-  return true;
+return true;
 }
 else {
-  return false;
+return false;
 }
   })
 }
@@ -240,15 +208,12 @@ generateKeyPair('rsa', {
  creates a transaction and forwards CIR to CA's on the blockchain for 
  cerificate signing and inclusion of the transaction on the ethereum blockchain */
 
+ // home route
  server.get('/', (req, res)=>{
   res.send({message: "Welcome Dr Abba Garba, use the url bbpki.herokuapp.com/signCertificate/<domainName> to sign the sertificate using the bls multisignature system, the signatures are responded back in a json format in your browser interface"})
   })
-  // routes 
-  server.get('/register-CA', async (req,res)=>{
-  // server-side register CA function call
-  registerCA();
-  })
   
+  // certificate issuance and signing route
   server.get('/sign-certificate/:domainName', async (req,res)=>{
     signCertificate(req.params.domainName);
     res.json({
@@ -258,21 +223,21 @@ generateKeyPair('rsa', {
     })
   })
   
+// cert revokation route
   server.get('/revoke-certificate', async (req,res)=>{
     revokeCertificate();
   })
   
+ // blockchain cert info route 
   server.get('/get-certificate', async (req,res)=>{
     getCertificate();
   })
   
+  // client verify cert using eth-proof route
   server.get('/client-verify-cert', async (req,res)=>{
     clientVerifyCert();
   })
   
-
-
-
 // starting developement/production server
 server.listen(process.env.PORT || 3000, (err)=>{
   if (err){
