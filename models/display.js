0a886a9f35845204efbc150fad4ca9b74467f025
Usman Danbaba <usmandanbaba258@yahoo.com>
2021-04-28 10:54:14 +0100

    add initial commit

--- models/display.js
+++ models/display.js
@@ -2,48 +2,56 @@ function log (certificate, numcas){
 if (numcas ==3){
     setTimeout(() => {
         console.log(certificate)
+        console.log('certificate has been signed by ', numcas, ' CAs')
     }, 5000);
 }
 
 if (numcas ==4){
     setTimeout(() => {
         console.log(certificate)
+        console.log('certificate has been signed by ', numcas, ' CAs')
     }, 6000);
 }
 
 if (numcas ==5){
     setTimeout(() => {
         console.log(certificate)
+        console.log('certificate has been signed by ', numcas, ' CAs')
     }, 7000);
 }
 
 if (numcas ==6){
     setTimeout(() => {
         console.log(certificate)
+        console.log('certificate has been signed by ', numcas, ' CAs')
     }, 8000);
 }
 
 if (numcas ==7){
     setTimeout(() => {
         console.log(certificate)
+        console.log('certificate has been signed by ', numcas, ' CAs')
     }, 10000);
 }
 
 if (numcas ==8){
     setTimeout(() => {
         console.log(certificate)
+        console.log('certificate has been signed by ', numcas, ' CAs')
     }, 11000);
 }
 
 if (numcas ==9){
     setTimeout(() => {
         console.log(certificate)
+        console.log('certificate has been signed by ', numcas, ' CAs')
     }, 12000);
 }
 
 if (numcas ==10){
     setTimeout(() => {
         console.log(certificate)
+        console.log('certificate has been signed by ', numcas, ' CAs')
     }, 12000);
 }
 }
